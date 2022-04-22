2dc4c929c0ecee3bc25bed3bca3e74d524b1404f
samin <bmst555m@gmail.com>
2022-04-22 10:02:00 +0430

    add media player

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.mediaplayer
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mediaplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initView()
+        setContentView(binding.root)
     }
+
+    private fun initView() {
+        val url = "https://dl.mix-music.ir/music/1400/01/The%20Pianist.mp3" // your URL here
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
+        }
+        binding.btn_play.setOnClickListener{
+            
+        }
+    }
+
+
 }
